9ae2b0865cb1c18f7728280eb7c41e969565ed4b
PC <nuhad7july02@gmail.com>
2025-04-29 23:51:56 +0600

    Fixed Some Bugs

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -110,8 +110,9 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {messages.ERROR: 'danger'}
 
 # Email settings (fake values for testing)
-EMAIL_HOST = 'smtp.example.com'
+EMAIL_HOST_USER = 'nuhad7july02@gmail.com'
+EMAIL_HOST_PASSWORD = 'duva valh ttda ffye'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'you@example.com'
-EMAIL_HOST_PASSWORD = 'yourpassword'
 EMAIL_USE_TLS = True
