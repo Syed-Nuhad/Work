55bb3495d5c4b5e369a0cc094ff5af82adec1ffc
PC <nuhad7july02@gmail.com>
2025-04-30 02:13:53 +0600

    Fixed Some Bugs

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -118,3 +118,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+
