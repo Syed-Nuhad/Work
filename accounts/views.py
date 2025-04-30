51ba13e9fd6c506c63f86397ccfe7dc5b4a71df0
PC <nuhad7july02@gmail.com>
2025-04-30 21:00:06 +0600

    Fixed Some Bugs

--- accounts/views.py
+++ accounts/views.py
@@ -150,8 +150,7 @@ def activate(request, uidb64, token):
 def dashboard(request):
     orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered=True)
     orders_count = orders.count()
-
-    userprofile = UserProfile.objects.get(user_id=request.user.id)
+    userprofile = UserProfile.objects.get_or_create(user_id=request.user.id)
     context = {
         'orders_count': orders_count,
         'userprofile': userprofile,
