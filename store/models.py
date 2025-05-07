981474815dee108be1c0bb84e0e9a504f7045e24
PC <nuhad7july02@gmail.com>
2025-05-08 03:34:24 +0600

    Fixed Some Bugs

--- store/models.py
+++ store/models.py
@@ -17,7 +17,6 @@ class Product(models.Model):
     category        = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_date    = models.DateTimeField(auto_now_add=True)
     modified_date   = models.DateTimeField(auto_now=True)
-    name = models.CharField(max_length=100)
     selling_price = models.DecimalField(decimal_places=2)
     cost_price = models.DecimalField(decimal_places=2)
     sold = models.PositiveIntegerField(default=0)
