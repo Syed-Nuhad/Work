7129bd00c1719347d5f5c38d1089946222c623fc
PC <nuhad7july02@gmail.com>
2025-05-07 16:37:18 +0600

    Fixed Some Bugs

--- accounts/models.py
+++ accounts/models.py
@@ -24,20 +24,21 @@ class MyAccountManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, first_name, last_name, email, username, password):
-        user = self.create_user(
-            email = self.normalize_email(email),
-            username = username,
-            password = password,
-            first_name = first_name,
-            last_name = last_name,
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('first_name', 'Admin')
+        extra_fields.setdefault('last_name', 'User')
+        extra_fields.setdefault('is_admin', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superadmin', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(
+            email=email,
+            username=username,
+            password=password,
+            **extra_fields
         )
-        user.is_admin = True
-        user.is_active = True
-        user.is_staff = True
-        user.is_superadmin = True
-        user.save(using=self._db)
-        return user
 
 
 class Account(AbstractBaseUser, PermissionsMixin):
