50309005cfa5cde8bec864ff81cbc3f5c4c7680f
PC <nuhad7july02@gmail.com>
2025-05-11 21:26:17 +0600

    Fixed Some Bugs

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 from django.urls import path, include
 from . import views
 def run_migrations(request):
-    call_command('migrate')
-    return HttpResponse("✅ Migrations complete.")
+    call_command('migrate', interactive=False)
+    return HttpResponse("✅ Migrations applied successfully.")
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
