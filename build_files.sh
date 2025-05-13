e46a7967e934465bbbc015d3d94c6b42ae5dead5
PC <nuhad7july02@gmail.com>
2025-05-13 21:39:00 +0600

    Fixed Some Bugs

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,9 @@
+python3 -m venv venv
+source venv/bin/activate
+
+# Install dependencies
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+
+# Collect static files and migrate
+python3.9 manage.py collectstatic --noinput
+python3.9 manage.py migrate
