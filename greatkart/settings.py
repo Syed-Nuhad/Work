bcad24f975748f825ba4f31eb52af8df3a48505f
PC <nuhad7july02@gmail.com>
2025-05-16 03:00:33 +0600

    Fixed Some Bugs

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -72,23 +72,7 @@ WSGI_APPLICATION = 'greatkart.wsgi.application'
 
 AUTH_USER_MODEL = 'accounts.Account'
 
-# Database setup
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-            'OPTIONS': {
-                'sslmode': 'require',
-            },
-        }
-    }
-else:
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'koyebdb',
